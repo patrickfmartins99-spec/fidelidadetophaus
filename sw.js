043104e421c161f71c1a0a3469d2256fc2ebb59c
patrickfmartins99-spec <patrickfmartins99@gmail.com>
2026-07-27 00:00:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tophaus-fidelidade-v46'; // Incrementei a versão para forçar a atualização
+const CACHE_NAME = 'tophaus-fidelidade-v47'; // Incrementei a versão para forçar a atualização
 
 const urlsToCache = [
   './',
