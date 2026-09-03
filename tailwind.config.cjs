846b047d9e2db24c4bffca40e6dbcce689414768
patrickfmartins99-spec <patrickfmartins99@gmail.com>
2026-09-03 12:47:46 -0300

    Infraestrutura segura: adicionar tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './*.js',
+    './fragments/**/*.html'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
+
